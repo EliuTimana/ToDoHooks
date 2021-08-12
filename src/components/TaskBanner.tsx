2319d49d495a2317fbf9fda5d49d06129cc46293
Eliú Timaná <EliuTimana@gmail.com>
2021-08-11 19:00:00 -0500

    🎨 STYLE: styled-components

--- src/components/TaskBanner.tsx
+++ src/components/TaskBanner.tsx
@@ -1,12 +1,21 @@
 import { useContext } from 'react';
+import styled from 'styled-components';
 import { ToDoContext } from '../context/ToDoContext';
-
+const H4 = styled.h4<{ completed: boolean }>`
+  box-sizing: border-box;
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+  font-weight: 500;
+  line-height: 1.2;
+  font-size: calc(1.275rem + 0.3vw);
+  padding: 1.5rem !important;
+  text-align: center !important;
+  color: #fff !important;
+  transition: background-color 0.25s;
+  background-color: ${(props) => (props.completed ? '#198754' : '#0d6efd')};
+`;
 export const TaskBanner = () => {
   const context = useContext(ToDoContext);
   const todos = context.tasks.filter((t) => !t.done).length;
-  return (
-    <h4 className={'text-white text-center p-4' + (todos === 0 ? ' bg-success' : ' bg-primary')}>
-      Tasks App ({todos} tasks to do)
-    </h4>
-  );
+  return <H4 completed={todos === 0}>Tasks App ({todos} tasks to do)</H4>;
 };
--- src/components/TaskRow.tsx
+++ src/components/TaskRow.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import styled, { css } from 'styled-components';
 import { ToDoContext } from '../context/ToDoContext';
 import { Task } from '../models/models';
 
@@ -6,18 +7,27 @@ interface Props {
   task: Task;
 }
 
+const StyledLi = styled.li<any>`
+  background-color: ${(props) => (props.dark ? '#343a40' : 'white')};
+  color: ${(props) => (props.dark ? 'white' : '#41464b')};
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: background-color 0.25s;
+  &:hover {
+    background-color: ${(props) => (props.dark ? '#212529' : '#f5f3f3')};
+  }
+`;
+
 export const TaskRow = ({ task }: Props) => {
   const context = useContext(ToDoContext);
   return (
-    <li
-      className={
-        'list-group-item d-flex justify-content-between align-items-center' +
-        (task.done ? ' list-group-item-secondary' : '')
-      }
-      style={{
-        backgroundColor: context.isDark ? '#343a40' : 'white',
-        color: context.isDark ? 'white' : '#41464b',
-      }}
+    <StyledLi
+      dark={context.isDark}
+      className={'list-group-item' + (task.done ? ' list-group-item-secondary' : '')}
+      onClick={() => context.toggleTask(task)}
     >
       <div>
         <input
@@ -31,6 +41,6 @@ export const TaskRow = ({ task }: Props) => {
       <button type="button" onClick={() => context.deleteTask(task)} className="btn btn-danger btn-sm">
         X
       </button>
-    </li>
+    </StyledLi>
   );
 };
