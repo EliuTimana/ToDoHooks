d0ee0ad49b3002d1e997359514a02da231152ea3
Eliú Timaná <EliuTimana@gmail.com>
2021-08-14 15:22:16 -0500

    📦 NEW: morgan package on server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
+
 const app = express();
 const port = 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('combined'));
 
 /**
  * @type {{id: number, description: string, done: boolean}[]} tasks
