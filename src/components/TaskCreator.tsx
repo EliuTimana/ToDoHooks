88ba15678e9c977c4e718dba34ba013234cb8c4f
Eliú Timaná <EliuTimana@gmail.com>
2021-08-14 15:23:10 -0500

    📦 NEW: create task with react query

--- src/components/TaskCreator.tsx
+++ src/components/TaskCreator.tsx
@@ -1,18 +1,26 @@
 import { Input } from '@rebass/forms';
 import { useContext, useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
 import { Box } from 'rebass';
+import { addTask } from '../api';
 import { ToDoContext } from '../context/ToDoContext';
 import { VisibilityToggler } from './VisibilityToggler';
 
 export const TaskCreator = () => {
   const context = useContext(ToDoContext);
   const [text, setText] = useState('');
+  const queryClient = useQueryClient();
+  const { mutate, isLoading, error } = useMutation('addTask', (text: string) => addTask(text), {
+    onSuccess: () => {
+      queryClient.invalidateQueries('listTasks');
+    },
+  });
 
   const onKeyUp = (key: string) => {
     if (key.toLowerCase() === 'enter') {
       const trimmedText = text.trim();
       if (trimmedText) {
-        context.addTask(trimmedText);
+        mutate(trimmedText);
         setText('');
       }
     }
@@ -32,8 +40,10 @@ export const TaskCreator = () => {
           value={text}
           onKeyUp={(e: any) => onKeyUp(e.key)}
           onChange={(e: any) => setText(e.target.value)}
+          disabled={isLoading}
         />
       </Box>
+      {error && <Box>{error + ''}</Box>}
       <Box>
         <VisibilityToggler />
       </Box>
